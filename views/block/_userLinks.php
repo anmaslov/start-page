b63647af4db4d017017ab31098d27d261e49170b
anmaslov <lelick29@gmail.com>
2015-04-19 10:15:32 +0300

    change link status only admin

--- views/block/_userLinks.php
+++ views/block/_userLinks.php
@@ -4,37 +4,45 @@ use dosamigos\editable\Editable;
 ?>
 
 <? if(count($model->links)>0):?>
-<h4>Список ссылок:</h4>
+<div class="panel panel-default">
+    <div class="panel-heading">Список ссылок:</div>
 
-<ul class="list-group">
-    <?foreach($model->links as $item):?>
-        <li class="list-group-item">
-            <?if(strlen($item->icon)>0):?>
-                <i class="glyphicon glyphicon-<?=$item->icon?>"></i>
-            <?endif?>
-            <?=$item['title']?>
+    <div class="panel-body">
+        <ul class="list-group">
+            <?foreach($model->links as $item):?>
+                <li class="list-group-item">
+                    <?if(strlen($item->icon)>0):?>
+                        <i class="glyphicon glyphicon-<?=$item->icon?>"></i>
+                    <?endif?>
+                    <?=$item['title']?>
 
-            <?= Editable::widget( [
-                'model' => $item,
-                'attribute' => 'status',
-                'url' => 'block/link',
-                'type' => 'select2',
-                'mode' => 'pop',
-                'options' => ['id' => 'link-status-'.$item->id],
-                'clientOptions' => [
-                    'pk' => $item->id,
-                    'autotext' => 'always',
-                    'placement' => 'right',
-                    'select2' => [
-                        'width' => '150px'
-                    ],
-                    'value' => $item->status,
-                    'source' => $item::getStatusesArrayValue(),
-                ]
-            ]);?>
-        </li>
-    <?endforeach?>
-</ul>
-
+                    <?if(Yii::$app->user->can('admin')):?>
+                        <?= Editable::widget( [
+                            'model' => $item,
+                            'attribute' => 'status',
+                            'url' => 'block/link',
+                            'type' => 'select2',
+                            'mode' => 'pop',
+                            'options' => [
+                                'id' => 'link-status-'.$item->id,
+                                'class' => 'pull-right',
+                            ],
+                            'clientOptions' => [
+                                'pk' => $item->id,
+                                'autotext' => 'always',
+                                'placement' => 'right',
+                                'select2' => [
+                                    'width' => '150px'
+                                ],
+                                'value' => $item->status,
+                                'source' => $item::getStatusesArrayValue(),
+                            ]
+                        ]);?>
+                    <?endif?>
+                </li>
+            <?endforeach?>
+        </ul>
+    </div>
+</div>
 
 <?endif?>
