10cff7ccce2a12fde79916bec1d2b76a58654e5e
lelick <lelick29@gmail.com>
2015-05-29 15:02:02 +0300

    yii2-autocomplete-widget

--- models/LinkStats.php
+++ models/LinkStats.php
@@ -36,7 +36,7 @@ class LinkStats extends \yii\db\ActiveRecord
          [
              'class' => TimestampBehavior::className(),
              'updatedAtAttribute' => 'created_at',
-             'value' => new Expression(date('Y-m-d H:i:s')),
+             'value' => new Expression('NOW()'),
          ],
         ];
     }
--- views/main/_subtext.php
+++ views/main/_subtext.php
@@ -2,6 +2,7 @@
 
 use yii\bootstrap\Html;
 use app\assets\AutocompleteAsset;
+use anmaslov\autocomplete\AutoComplete;
 
 AutocompleteAsset::register($this);
 ?>
@@ -11,28 +12,18 @@ AutocompleteAsset::register($this);
         <h1><?=\Yii::$app->settings->get('brand.brand-sub-text')?></h1>
     </div>
     <div class="col-md-6">
-        <form>
-            <div class="form-group">
-                <?=Html::textInput('links', null, [
-                    'id' => 'autocomplete',
-                    'class' => 'form-control',
-                    'placeholder' => Yii::t('app', 'FAST_SEARCH')
-                ]) ?>
-            </div>
-        </form>
+        <?= AutoComplete::widget([
+                'name' => 'link',
+                'id' => 'super',
+                'data' =>  $link,
+                'options' => [
+                    'placeholder' => Yii::t('app', 'FAST_SEARCH'),
+                ],
+                'clientOptions' => [
+                    'minChars' => 2,
+                    'groupBy' => 'category',
+                ],
+            ])?>
+
     </div>
 </div>
-
-<script type="text/javascript">
-    $(function(){
-        var countries = <?= \yii\helpers\Json::encode($link);?>;
-        $('#autocomplete').autocomplete({
-            lookup: countries,
-            minChars: 2,
-            groupBy: 'category',
-            onSelect: function (suggestion) {
-                location.href = suggestion.stat;
-            }
-        }).focus();
-    });
-</script>
