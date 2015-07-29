9577dffaf36247e7713fd2fa97d17bbbb0e1e899
lelick <lelick29@gmail.com>
2015-07-29 10:39:32 +0300

    change diagramm to pyramid style

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -88,7 +88,7 @@ class SiteController extends Controller
     {
         Yii::$app->user->logout();
 
-        //return $this->goHome();
+        return $this->goHome();
     }
 
     public function actionContact()
--- views/stat/_stylestat.php
+++ views/stat/_stylestat.php
@@ -9,28 +9,16 @@ $this->registerJs('$.getJSON("'.URL::to(['stat/style']).'", dataStyle);');
 
 echo Highcharts::widget([
     'callback' => 'dataStyle',
+    'scripts' => [
+        'modules/funnel',
+    ],
     'options' => [
+        'chart' => ['type' => 'pyramid'],
         'title' => ['text' => Yii::t('app', 'STAT_DISTRIBUTION')],
         'colors' => ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
-        'plotOptions' =>[
-            'pie' => [
-                'allowPointSelect' => 'true',
-                'cursor' => 'pointer',
-                'depth' => 35,
-                'dataLabels' => [
-                    'enabled' => 'false'
-                ]
-            ]
-        ],
         'series' => [[
-            'type' => 'pie',
             'name' => Yii::t('app', 'STAT_COUNT'),
             'data' => new JsExpression('data')
-            /*'data' => [
-                ['name' => 'ubuntu', 'y' => 45, 'sliced' => true],
-                ['name' => 'default', 'y' => 6, 'sliced' => true],
-                ['name' => 'test', 'y' => 45],
-            ]*/
         ]]
     ]
 ]);
